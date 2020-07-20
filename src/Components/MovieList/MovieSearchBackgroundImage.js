6b5c79536f819604d77f8a67c9fcc449faf097cc
ichater <chaterizaak@gmail.com>
2020-07-20 19:42:33 +1000

    started changing movie scene background

--- src/Components/MovieList/MovieSearchBackgroundImage.js
+++ src/Components/MovieList/MovieSearchBackgroundImage.js
@@ -20,33 +20,36 @@ function useInterval(callback, delay) {
   }, [delay]);
 }
 
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
 export default function MovieSearchBackgroundImage() {
-  const backgroundImages = [
+  const posterArr = [
     "once-upon-a-time-in-america-1530579166.jpg",
     "thumb-1920-691866.jpg",
-    "hello",
+    "Cassablanca.jpg",
+    "_methode_times_prod_web_bin_265bd31e-cfdb-11e9-bac7-3a108794927b.jpg",
+    "Seventh_Seal.jpg",
+    "Ghost_in_the_shell.jpg",
+    "exorcist.jpg",
+    "Luke_Skywalker.jpeg",
+    "Mclane.jpg",
+    "rosemarys-baby-0618-ss08.jpg",
+    "Woody-Allen-Diane-Keaton-Oscar-Annie-Hall.jpg",
+    "Manhattan.jpg",
   ];
-  const [backgroundImage, setBackgroundImage] = useState(0);
+  const [backgroundImage, setBackgroundImage] = useState(posterArr[0]);
 
   useInterval(() => {
-    const funct = () => {
-      console.log(backgroundImage);
-
-      if (backgroundImage === backgroundImages.length) {
-        setBackgroundImage(backgroundImages[0]);
-      } else {
-        setBackgroundImage(backgroundImages[+1]);
-      }
-    };
-    funct();
+    setBackgroundImage(posterArr[getRandomInt(posterArr.length)]);
   }, 1000);
 
   return (
     <>
-      <div>{backgroundImage}</div>
       <img
         className="movie-search-image"
-        src={require("../../CSS/images/once-upon-a-time-in-america-1530579166.jpg")}
+        src={require(`../../CSS/images/${backgroundImage}`)}
       />
     </>
   );
