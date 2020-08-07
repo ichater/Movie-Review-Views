0f33d01b8b9fb4b783bbdbe39619fc2c0086efd8
ichater <chaterizaak@gmail.com>
2020-08-08 09:35:58 +1000

    started to work on Register form group

--- src/Components/LogIn/Register.js
+++ src/Components/LogIn/Register.js
@@ -1,27 +1,62 @@
-import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Register() {
+  const [registerData, setRegisterData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { username, email, password, password2 } = registerData;
+
+  const onChange = (e) =>
+    setRegisterData({ ...registerData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("passwords do not match");
+    } else {
+      console.log(registerData);
+    }
+  };
+
   return (
     <div className="user-form-group_wrapper">
       {" "}
-      <form className="register-form-group" action="/register" method="POST">
+      <form
+        className="register-form-group"
+        action="/register"
+        // method="POST"
+        onSubmit={(e) => onSubmit(e)}
+      >
         <div className="user-form-group">
           <label className="user-label" for="userName">
-            User Name:{" "}
+            Username:{" "}
           </label>
           <input
             className="user-input"
             type="text"
-            id="userName"
-            name="userName"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => onChange(e)}
           />
         </div>
         <div className="user-form-group">
           <label className="user-label" for="email">
             Email:{" "}
           </label>
-          <input className="user-input" type="text" id="email" name="email" />
+          <input
+            className="user-input"
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
         </div>
         <div className="user-form-group">
           <label className="user-label" for="password">
@@ -32,11 +67,24 @@ export default function Register() {
             type="text"
             id="password"
             name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
           />
         </div>
-        <div className="login-register-btn" type="submit">
-          Register
+        <div className="user-form-group">
+          <label className="user-label" for="password">
+            Confirm Password:{" "}
+          </label>
+          <input
+            className="user-input"
+            type="text"
+            id="password2"
+            name="password2"
+            value={password2}
+            onChange={(e) => onChange(e)}
+          />
         </div>
+        <input className="login-register-btn" type="submit" value="Register" />
       </form>
       <Link to="/signin"> Log In </Link>
     </div>
