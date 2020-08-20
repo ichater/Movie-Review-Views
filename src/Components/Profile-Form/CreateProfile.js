6a74c3d9ec41124e8925f91145f6038c1bbdbedd
ichater <chaterizaak@gmail.com>
2020-08-20 16:52:38 +1000

    added state and setState to inputs in update profile

--- src/Components/Profile-Form/CreateProfile.js
+++ src/Components/Profile-Form/CreateProfile.js
@@ -3,23 +3,69 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 const CreateProfile = (props) => {
-  const [formDate, setFormData] = useState();
+  const [formData, setFormData] = useState({
+    location: "",
+    description: "",
+    likesAboutMovies: "",
+    filmQuotes: {
+      quote: "",
+      film: "",
+    },
+  });
+
+  const {
+    location,
+    description,
+    likesAboutMovies,
+
+    quote,
+    film,
+  } = formData;
+
+  const onChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
   return (
     <div className="create-profile-form-wrapper">
       <form>
         <h3 className="profile-input-header">About you</h3>
         <div className="profile-form-group">
           <label className="profile-label">Location:</label>
-          <input typx="text" className="profile-input"></input>
+          <input
+            value={location}
+            name="location"
+            onChange={(e) => onChange(e)}
+            typx="text"
+            className="profile-input"
+          ></input>
         </div>
         <div className="profile-form-group">
           <label className="profile-label">Description:</label>
-          <textarea className="profile-input" rows="5" cols="50" />
+          <textarea
+            value={description}
+            name="description"
+            onChange={(e) => onChange(e)}
+            className="profile-input"
+            rows="5"
+            cols="50"
+          />
         </div>
         <div className="profile-form-group">
           <label className="profile-label">Likes About Movies:</label>
-          <textarea className="profile-input" rows="5" cols="50" />
+          <textarea
+            value={likesAboutMovies}
+            name="likesAboutMovies"
+            placeholder="Speak from the heart"
+            onChange={(e) => onChange(e)}
+            className="profile-input"
+            rows="5"
+            cols="50"
+          />
         </div>
+        <div className="profile-input-button">Add/Update Profile</div>
       </form>
       <form>
         <h3 className="profile-input-header">Add a Film Quote</h3>
@@ -27,14 +73,26 @@ const CreateProfile = (props) => {
           <label label className="profile-label">
             Film
           </label>
-          <input className="profile-input" />
+          <input
+            value={film}
+            placeholder={film}
+            name="film"
+            onChange={(e) => onChange(e)}
+            className="profile-input"
+          />
         </div>
         <div className="profile-form-group">
           <label label className="profile-label">
             Quote
           </label>
-          <input className="profile-input" />
+          <input
+            value={quote}
+            name="quote"
+            onChange={(e) => onChange(e)}
+            className="profile-input"
+          />
         </div>
+        <div className="profile-input-button">Add a quote</div>
       </form>
     </div>
   );
