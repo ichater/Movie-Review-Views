e93ad74dc98cfea1ab6a95beb21a1cf972b96ade
ichater <chaterizaak@gmail.com>
2020-08-22 11:40:20 +1000

    added logic for create/edit profile actions

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -18,3 +18,39 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or update a profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/profile", formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"));
+
+    //redirect to dashboard if not editing profile
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((err) => dispatch(setAlert(err.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
