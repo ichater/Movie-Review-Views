0e3aa248c534816628c114a2f993a699483004d8
ichater <chaterizaak@gmail.com>
2020-08-26 18:44:39 +1000

    successfully installed emotion

--- src/Components/Profile/ProfileTop.js
+++ src/Components/Profile/ProfileTop.js
@@ -1,10 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const ProfileTop = ({ profile }) => {
   return <div>Place Holder for main profile (Avatar to be added)</div>;
 };
-
-ProfileTop.propTypes = {};
 
 export default ProfileTop;
--- src/Components/Profiles/Profiles.js
+++ src/Components/Profiles/Profiles.js
@@ -4,27 +4,38 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfiles } from "../../actions/profile";
 import ProfileItem from "./ProfileItem";
-
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
   }, [getProfiles]);
+
+  const mainDisplayDiv = css`
+    width: 60%;
+    margin: auto;
+    background-color: rgba(102, 255, 102, 0.8);
+  `;
+
   return (
     <Fragment>
       {loading ? (
         <Spinner />
       ) : (
         <Fragment>
-          <h1> Profiles </h1>
-          <p>Browse other film viewers profiles</p>
-          <div>
-            {profiles.length > 0 ? (
-              profiles.map((profile) => (
-                <ProfileItem key={profile._id} profile={profile} />
-              ))
-            ) : (
-              <h4>No Profiles Found </h4>
-            )}
+          <div css={mainDisplayDiv}>
+            <h1> Profiles </h1>
+            <p>Browse other film viewers profiles</p>
+            <div>
+              {profiles.length > 0 ? (
+                profiles.map((profile) => (
+                  <ProfileItem key={profile._id} profile={profile} />
+                ))
+              ) : (
+                <h4>No Profiles Found </h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
